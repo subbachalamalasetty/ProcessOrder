534bca44881b009bc2080b2efd5754817b40c803
subbachalamalasetty <76822805+subbachalamalasetty@users.noreply.github.com>
2021-01-01 12:34:19 -0600

    Add files via upload

---
+++ ProcessOrder.Tests/TestContextHelper.cs
@@ -0,0 +1,60 @@
+﻿using ProcessOrder.Infrastructure.DataContext;
+using ProcessOrder.Infrastructure.DataContext.TestConfiguration;
+using System.Collections.Generic;
+
+namespace ProcessOrder.Tests
+{
+    internal static class TestContextHelper
+    {
+        public static ProcessOrderDbContext GetContext()
+        {
+            var processOrderContextFactory = new ProcessOrderContextFactoryForTest();
+            var writableContext = processOrderContextFactory.CreateWriteableContext();
+            return writableContext;
+        }
+
+        public static ProcessOrderDbContext GetCleanContext()
+        {
+            CleanDatabase();
+            var processOrderContextFactory = new ProcessOrderContextFactoryForTest();
+            var writableContext = processOrderContextFactory.CreateWriteableContext();
+            return writableContext;
+        }
+
+        public static void CleanDatabase()
+        {
+            var processOrderContextFactory = new ProcessOrderContextFactoryForTest();
+            var writableContext = processOrderContextFactory.CreateWriteableContext();
+
+            writableContext.ResetValueGenerators();
+            writableContext.Database.EnsureDeleted();
+        }
+
+        public static void AddEntity<T>(T entity) where T : class
+        {
+            var context = GetContext();
+            context.Set<T>().AddRange(entity);
+            context.SaveChanges();
+        }
+
+        public static void AddEntity<T>(ProcessOrderDbContext context, T entity) where T : class
+        {
+            context.Set<T>().AddRange(entity);
+            context.SaveChanges();
+        }
+
+        public static void AddEntities<T>(List<T> entities) where T : class
+        {
+            var context = GetContext();
+            context.Set<T>().AddRange(entities);
+            context.SaveChanges();
+        }
+
+        public static void AddEntities<T>(ProcessOrderDbContext context, List<T> entities) where T : class
+        {
+            context.Set<T>().AddRange(entities);
+            context.SaveChanges();
+        }
+    }
+}
+
