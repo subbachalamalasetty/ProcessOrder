5d2766367c723d406e668fabdf93cea3ffcf876a
subbachalamalasetty <76822805+subbachalamalasetty@users.noreply.github.com>
2021-01-01 12:10:21 -0600

    Add files via upload

---
+++ ProcessOrder.API/ProcessOrder.cs
@@ -0,0 +1,36 @@
+﻿using ProcessOrder.Core;
+using ProcessOrder.Infrastructure.Models;
+using System.Threading.Tasks;
+
+namespace ProcessOrder.API
+{
+    public class ProcessOrder
+    {
+        private readonly IProcessOrderService _processOrderService;
+        public ProcessOrder(IProcessOrderService processOrderService)
+        {
+            _processOrderService = processOrderService;
+        }
+
+        public async Task Process(string productId, decimal amount, int qty, string creditCardNumber)
+        {
+            var order = new Order
+            {
+                Amount = amount,
+                Product = new Product
+                {
+                    ProductId = productId
+                },
+            };
+
+            var userOrder = new UserOrder
+            {
+                UserName = "Bob",
+                UserAddress = "addr-1, test-city, test-state-99999",
+                OrderByUser = order
+            };
+
+            await _processOrderService.Process(userOrder, qty, creditCardNumber);
+        }
+    }
+}
