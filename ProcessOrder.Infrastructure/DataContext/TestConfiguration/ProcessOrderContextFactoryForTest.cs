70f74c6764fd9c057dc0d355f95ebc9b4cf1419c
subbachalamalasetty <76822805+subbachalamalasetty@users.noreply.github.com>
2021-01-01 12:19:31 -0600

    Add files via upload

---
+++ ProcessOrder.Infrastructure/DataContext/TestConfiguration/ProcessOrderContextFactoryForTest.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace ProcessOrder.Infrastructure.DataContext.TestConfiguration
+{
+    public class ProcessOrderContextFactoryForTest : IProcessOrderContextFactory
+    {
+        private static readonly InMemoryDatabaseRoot InMemoryDatabaseRoot;
+
+        static ProcessOrderContextFactoryForTest()
+        {
+            InMemoryDatabaseRoot = new InMemoryDatabaseRoot();
+        }
+
+        public ReadOnlyProcessOrderContext CreateReadOnlyContext()
+        {
+            return CreateReadOnlyProcessOrderContext();
+        }
+
+        public ProcessOrderDbContext CreateWriteableContext()
+        {
+            return CreateProcessOrderContext();
+        }
+
+        private static ProcessOrderDbContext CreateProcessOrderContext()
+        {
+            var options = new DbContextOptionsBuilder<ProcessOrderDbContext>()
+                .UseInMemoryDatabase("ProcessOrder", InMemoryDatabaseRoot)
+                .ConfigureWarnings(w => w.Ignore(InMemoryEventId.TransactionIgnoredWarning))
+                .Options;
+
+            var processOrderContext = new ProcessOrderDbContext(options);
+
+            processOrderContext.SetAllValueGeneratorsAsResettable();
+
+            return processOrderContext;
+        }
+
+        private static ReadOnlyProcessOrderContext CreateReadOnlyProcessOrderContext()
+        {
+            var options = new DbContextOptionsBuilder<ProcessOrderDbContext>()
+                .UseInMemoryDatabase("ProcessOrder", InMemoryDatabaseRoot)
+                .ConfigureWarnings(w => w.Ignore(InMemoryEventId.TransactionIgnoredWarning))
+                .Options;
+
+            var processOrderContext = new ReadOnlyProcessOrderContext(options);
+
+            processOrderContext.SetAllValueGeneratorsAsResettable();
+
+            return processOrderContext;
+        }
+
+    }
+}
---
+++ ProcessOrder.Infrastructure/DataContext/TestConfiguration/ResettableValueGenerator.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.ValueGeneration;
+using System.Threading;
+
+namespace ProcessOrder.Infrastructure.DataContext.TestConfiguration
+{
+    public class ResettableValueGenerator : ValueGenerator<int>
+    {
+        private int _current;
+
+        public override bool GeneratesTemporaryValues => false;
+
+        public override int Next(EntityEntry entry)
+            => Interlocked.Increment(ref _current);
+
+        public void Reset() => _current = 0;
+    }
+}
