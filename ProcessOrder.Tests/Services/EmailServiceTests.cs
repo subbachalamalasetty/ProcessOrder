2527da313f9d15975b22c6b28ab49b8fe144e663
subbachalamalasetty <76822805+subbachalamalasetty@users.noreply.github.com>
2021-01-01 12:32:51 -0600

    Add files via upload

---
+++ ProcessOrder.Tests/Services/EmailServiceTests.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProcessOrder.Tests.Services
+{
+    /// <summary>
+    /// TODO: Can add tests to EmailService
+    /// </summary>
+    [TestClass]
+    public class EmailServiceTests
+    {
+    }
+}
